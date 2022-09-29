c882d2f77d03a3d891e83a8194a1106b5f1be735
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2022-09-29 01:23:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length >5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="search here..."/>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
