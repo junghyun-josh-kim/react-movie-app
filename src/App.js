aed6c34039b83fd45973b77a1a8b0560c4eec4b5
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2022-09-29 14:49:25 -0400

    React: Recapping
    useEffect, useState recapping

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("Hi :)");
+    return () => console.log("Bye :(");
   }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length >5) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev);
   return (
-    <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="search here..."/>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
+  <div>
+    {showing ? <Hello /> : null}
+    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+  </div>
   );
 }
 
